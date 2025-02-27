876aeb1420b6b51b727602edcbd7ffd9309cb7dc
1122gs <GORDON.SMITHJR@hotmail.com>
2025-02-27 01:51:40 +0000

    final commit

--- src/models.py
+++ src/models.py
@@ -6,27 +6,47 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped["Address"] = relationship(back_populates="person")
+    user_name: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(nullable = True) #required = True
+    password: Mapped[str] = mapped_column(nullable = True) #required = True
 
 
-class Address(Base):
-    __tablename__ = 'address'
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
-    person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
-    person: Mapped["Person"] = relationship(back_populates="address")
+    user_favorite : Mapped[int] = mapped(ForeignKey("user.id"))
+    planet_favorite: Mapped[int] = mapped_column(ForeignKey("planet.id"))
+    character_favorite: Mapped[int] = mapped_column(ForeignKey('character.id'))
 
-    def to_dict(self):
+
+class Character(Base):
+        __tablename__ = 'character'
+        id: Mapped[int] = mapped_column(primary_key=True)
+        name: Mapped[str] = mapped_column(nullable=False)
+        height: Mapped[int] = mapped_column(nullable=False)
+        eye_color = Mapped[str]
+        gender: Mapped[str]
+
+
+class Planet(Base):
+        __tablename__ = 'planet'
+        id: Mapped[int] = mapped_column(primary_key=True)
+        population: Mapped[str] = mapped_column(nullable=False)
+        size: Mapped[str] = mapped_column(nullable=False)
+        gas: Mapped[str]
+
+        
+    
+
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
